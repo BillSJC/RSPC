5a492d7d68ff4aabf2a7b5e5a40ccef5bcff1284
sjc <707425690@qq.com>
2018-09-25 08:57:25 +0800

    edit welcome msg

--- backend/application/views/welcome_message.php
+++ backend/application/views/welcome_message.php
@@ -71,15 +71,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<h1>Welcome to CodeIgniter!</h1>
 
 	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+		<p>PSPC 公选课推荐系统.</p>
 
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+		<p>这是一个正在建设的前端</p>
+		<code>稍等片刻他或许就会上线</code>
+		<p>此处有<a href="user_guide/">https://github.com/BillSJC/RSPC</a>.</p>
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
