2a2dee183812f5c71c0f1db01a81c5e8edb1f140
sjc <707425690@qq.com>
2018-09-27 00:06:49 +0800

    init ajax request

--- frontend/func.js
+++ frontend/func.js
@@ -8,6 +8,42 @@ function getQueryVariable(variable){
        return(false);
 }
 
-function ajaxSendResuestToAPI(u){
-    
+function ajaxSendResuestToAPI(tragetUrl,handler){
+    token = localStorage.getItem("accessToken")
+    if(token == null){
+            alert("您还未登录，请先登录！");
+            window.location("https://rspc.hduhelp.com/login.html");
+            return -1;
+    }
+    resp = $.ajax({
+        url:tragetUrl,
+        async:false,
+        type:"get",
+        headers: {
+                "Authorization":"token "+token
+        },
+        success: function(data) {
+                handler(data);
+                return 1;
+        },
+        error: function(err) {
+                console.log("error in ajax:"+error)
+                return 0;
+        },
+    })
+}
+
+function checkToken(){
+        if(getToken()==null){
+                return false;
+        }
+        return true;
+}
+
+function getToken(){
+        return localStorage.getItem("accessToken");
+}
+
+function insertToken(token){
+        localStorage.setItem("accessToken",token);
 }
